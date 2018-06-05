73aca0a8eba16493c057290c58b75ca7cad06ba9
jingyongwang <lovedpotato@gmail.com>
2018-06-05 17:24:17 +0800

    Nav and Board

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,68 +1,82 @@
 import React, { Component } from 'react';
-import styled, {css} from 'styled-components'
-
+import styled, {css} from 'styled-components';
+// import {BoardPanel} from './Board';
+// import {DesktopNavigation} from './Nav'
 import './App.css';
 
 
-
+const GridWrapper = styled.div ``;
 const NavContainer = styled.div ``;
+const NavItem = styled.button ``;
 
-const NavItem = styled.div `
-  position: relative;
-`;
 
-const DesktopNav = styled.nav `
-  background-color : #4A90E2;
-  width : 1024px;
+
+const NavWrapper = styled.nav `
+ background-color : #4A90E2;
+  width : 100%;
   height : 78px;
   
-  ${NavContainer} {
+  ${GridWrapper} {
     display: flex;
     align-items: center;
     
     ${NavItem} {
-      margin: 0 10px;
       text-align: center;
-    }
-    a {
-      margin-left : 25px;
+      margin: 0 120px;
+      
     }
   };
 
-
 `;
 
+const BoardContainer = styled.div `
+  background-color : gray;
+  width: 500px;
+  height: 123px;
 
-
-
-
+`;
+const BoardContent = styled.div ``;
+const BoardTitle = styled.div ``;
+const BoardRectangle = styled.div ``;
 
 class DesktopNavigation extends React.Component {
-  // constructor (props) {
-  //   super(props)
-  
-  // }
+
+  constructor(props) {
+    super(props);
+  }
   
   render() {
     return (
-      <DesktopNav>
-        Nav
-        <NavContainer>
-          <NavItem>
-            <a>My Task</a>
-          </NavItem>
-          <NavItem>
-            <a>In Progress</a>
-          </NavItem>
-          <NavItem>
-            <a>Completed</a>
-          </NavItem>
-        </NavContainer>
-      </DesktopNav>
+      <NavWrapper>
+        <GridWrapper>
+          <NavContainer>
+            <NavItem>My Tasks</NavItem>
+            <NavItem>In Progress</NavItem>
+            <NavItem>Completed</NavItem>
+
+          </NavContainer>
+        </GridWrapper>
+      </NavWrapper>
     );
   }
-
 }
 
 
+const BoardPanel = () => (
+  <BoardPanel>
+    <BoardContainer>
+      <BoardContent>
+      <BoardTitle>
+        Type something here
+      </BoardTitle>
+
+      </BoardContent>
+      <BoardRectangle>
+      </BoardRectangle>
+    </BoardContainer>
+  </BoardPanel>
+);
+
+
 export default DesktopNavigation;
+// export default {DesktopNavigation, BoardPanel }
